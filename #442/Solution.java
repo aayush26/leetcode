b1368cf86de86c86d726801fe82b87019995ca30
mpeters <mpeters@infoverity.com>
2019-10-11 12:32:57 -0400

    #442 solution

---
+++ #442/Solution.java
@@ -0,0 +1,17 @@
+/**
+ * https://leetcode.com/problems/find-all-duplicates-in-an-array/
+ */
+
+class Solution {
+	public List<Integer> findDuplicates(int[] nums) {
+		List<Integer> duplicates = new ArrayList<Integer>();
+		Set<Integer> noDupes = new HashSet<Integer>();
+        for (int i = 0; i < nums.length; i++){
+            boolean isAdded = noDupes.add(nums[i]);
+            if(isAdded == false) {
+            	duplicates.add(nums[i]);
+            }
+        }
+        return duplicates;
+	}
+}
