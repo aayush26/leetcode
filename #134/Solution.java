f3db05d4652ea2e548a8a732a0bd24a09e44f88d
Ashutosh Singh <officiallyashu@gmail.com>
2019-10-13 03:16:36 +0530

    code added for #134 https://github.com/aayush26/leetcode/issues/21

---
+++ #134/Solution.java
@@ -0,0 +1,35 @@
+/*
+ * https://leetcode.com/problems/gas-station/
+*/
+
+class Solution
+{
+    int gas[];
+    int cost[];
+    
+    public int canCompleteCircuit(int gas[], int cost[]) {
+	int sumRemaining = 0; // track current remaining
+	int total = 0; // track total remaining
+	int start = 0; //starting
+ 
+	for (int i = 0; i < gas.length; i++) {
+		int remaining = gas[i] - cost[i];
+ 
+		//if sum remaining of (i-1) >= 0, continue 
+		if (sumRemaining >= 0) { 
+			sumRemaining += remaining;
+		//otherwise, reset start index to be current
+		} else {
+			sumRemaining = remaining;
+			start = i;
+		}
+		total += remaining;
+	}
+ 
+	if (total >= 0){
+		return start;
+	}else{
+		return -1;
+	}
+}
+}
