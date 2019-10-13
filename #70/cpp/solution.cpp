a7563f2c88416f709e0cd15afd31c92c0aa71c31
Manilal Kasera <manihereatcusat@gmail.com>
2019-10-13 08:55:53 +0530

    memory beats changes

--- #70/cpp/solution.cpp
+++ #70/cpp/solution.cpp
@@ -1,7 +1,7 @@
 /**
  * https://leetcode.com/problems/climbing-stairs/
  * runtime beats 51.66% of cpp solutions
- * memory usage beats 97.06% of cpp solutions
+ * memory usage beats 33.82% of cpp solutions
 **
 
 class Solution {
