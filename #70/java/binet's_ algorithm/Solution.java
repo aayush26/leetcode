e77623a5a82baca449ca6180050472e27ae69930
Ajanthasingam Jegasingam <ajantahsingam@wso2.com>
2019-10-13 07:28:08 +0530

    Rearrange the file system to follow the contribution guide

--- #70/java/binet's_ algorithm/Solution.java
+++ #70/java/binet's_ algorithm/Solution.java
@@ -1,7 +1,11 @@
+/**
+ * https://leetcode.com/problems/climbing-stairs/
+ * Runtime: 0 ms, faster than 100.00% of Java online submissions for Climbing Stairs.
+ * Memory Usage: 33.1 MB, less than 5.26% of Java online submissions for Climbing Stairs.
+ */
+
 public class Solution {
         public int climbStairs(int n) {
-            //Using Binet's solution for Fibonacci series
-            // Time complexity  O(1)
 
             double rootFive = Math.sqrt(5);
             double phi = (1+rootFive)/2;
