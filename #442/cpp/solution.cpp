3432608e52dce890dc8dd071dba49b21e250dcd7
rpartha <tilluboy@gmail.com>
2019-10-14 16:14:11 -0400

    [442] Added cpp solution

---
+++ #442/cpp/solution.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> findDuplicates(vector<int>& nums) {
+        int size = nums.size();
+        vector<int> dups;
+
+        for(int i = 0; i < size;)
+            if(nums[nums[i]-1] != nums[i])
+                swap(nums[i], nums[nums[i]-1]);
+            else i++;
+
+        for(int i = 0;i < size;i++)
+            if(nums[i]!=i+1) 
+                dups.push_back(nums[i]);
+                
+        return dups;
+    }
+};
