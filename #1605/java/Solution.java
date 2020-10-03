9bcf48bd395dcba89fff3412bc30c55f5ac62285
Ruchira Gothankar <ruchira1802@gmail.com>
2020-10-04 01:22:00 +0530

    Create Solution.java (#51)

---
+++ #1605/java/Solution.java
@@ -0,0 +1,16 @@
+/**
+* https://leetcode.com/problems/find-valid-matrix-given-row-and-column-sums/
+*/
+class Solution {
+    public int[][] restoreMatrix(int[] rowSum, int[] colSum) {
+        int[][] res = new int[rowSum.length][colSum.length];
+        for(int i=0;i<rowSum.length; i++){
+            for(int j=0;j<colSum.length; j++){
+                res[i][j] = Math.min(rowSum[i], colSum[j]);
+                rowSum[i] -= res[i][j];
+                colSum[j] -= res[i][j];
+            }
+        }
+        return res;
+    }
+}
